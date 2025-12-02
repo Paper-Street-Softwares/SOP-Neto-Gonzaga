ab9ff083b28c831ae3bd2bc056b12aa4f389d53f
Gabriel Souza <gabrielsouza65115@gmail.com>
2025-12-02 15:24:51 -0300

    fix: Copy alterado Features

--- src/components/sectionElements/footer/EmailFooter.jsx
+++ src/components/sectionElements/footer/EmailFooter.jsx
@@ -4,14 +4,14 @@ import MotionDivDownToUp from "../../animation/MotionDivDownToUp";
 import content from "../../../content/content";
 
 function EmailFooter({
-  emailTwo = false,
+  emailTwo = true,
   emailThree = false,
   emailFour = false,
   emailFive = false,
 }) {
   return (
     <>
-      <MotionDivDownToUp>
+      {/* <MotionDivDownToUp>
         <div className="flex full gap-x-[12px] items-center opacity-90">
           <span>
             <Mail />
@@ -19,7 +19,7 @@ function EmailFooter({
 
           <p className="text-paragraph3">{content.texts.infos.email}</p>
         </div>
-      </MotionDivDownToUp>
+      </MotionDivDownToUp> */}
 
       {emailTwo && (
         <MotionDivDownToUp>
--- src/components/sections/NavbarSocial.jsx
+++ src/components/sections/NavbarSocial.jsx
@@ -116,8 +116,8 @@ export default function NavbarSocial({ colorMode, mode }) {
               alt={content.texts.navbar.logo.alt}
               className={`${
                 scrolling
-                  ? "w-[40%] tablet1:w-[40%] tablet2:w-[40%] desktop1:w-[30%] desktop2:w-[30%] transition-all duration-1000 "
-                  : "my-[20px] w-[50%] phone3:w-[50%] tablet1:w-[55%] tablet2:w-[50%] desktop1:w-[40%] desktop2:w-[50%] transition-all duration-1000 "
+                  ? "w-[40%] tablet1:w-[40%] tablet2:w-[40%] desktop1:w-[40%] desktop2:w-[50%] transition-all duration-1000 "
+                  : "my-[20px] w-[50%] phone3:w-[50%] tablet1:w-[60%] tablet2:w-[70%] desktop1:w-[60%] desktop2:w-[70%] transition-all duration-1000 "
               } tablet3:mb-0`}
             />
           </ScrollLink>
